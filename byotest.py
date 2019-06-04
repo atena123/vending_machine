6b7c0adac0ffacb5aa15b2aaf0ad5bbd4a3c955b
Aneta Kobylarz <atena771@wp.pl>
2019-06-04 18:48:44 +0000

    boytest.py file

---
+++ byotest.py
@@ -0,0 +1,25 @@
+
+def test_are_equal(actual, expected):
+    assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
+
+
+def test_not_equal(a, b):
+    assert a != b, "Did not expect {0} but got {1}".format(a, b)
+
+
+def test_is_in(collection, item):
+    assert item in collection, "{0} does not contain {1}".format(collection, item)
+
+
+def test_not_in(collection, item):
+    assert item in collection, "{0} is not in one {1}".format(item, collection)
+    
+    
+def test_between(upper_limit, lower_limit, actual):
+    assert lower_limit <= actual <= upper_limit, "{0} is not between {1} and {2}".format(actual, lower_limit, upper_limit)
+    
+    
+    
+    
+    
+    
